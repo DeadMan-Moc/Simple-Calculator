0c974808eaa0fb684d325df61db65e60a189db71
Alfred Mokgohloa <45788338+DeadMan-Moc@users.noreply.github.com>
2019-09-13 11:04:37 +0200

    fresh

--- src/add.js
+++ src/add.js
@@ -1,12 +1,62 @@
-function add(){
-    
-    let sum = 0;
-    
+class Calculator{
 
-    for (let i = 0; i < arguments.length; i++) {
-        sum += arguments[i];
+    constructor(){
+      this.arr = [];
+      this.lastAns = 0;
     }
-    return sum;
-}
+  
+    add(...args){
+  
+      let i = 0;
+      let sum = 0;
+      for (; i < args.length; i++){
+        if (args[i] === "LAST"){
+          args[i] = this.lastAns;
+        }
+        if (args[i] === "SLOT_1"){
+          args[i] = this.get_slot(1);
+        }
+        if (args[i] === "SLOT_2"){
+          args[i] = this.get_slot(2);
+        }
+        sum = sum + args[i];
+      }
+      this.lastAns = sum;
+      return sum;
+    }
+  
+    multiply(...args){
+  
+      let i = 0;
+      let product = 1;
+      for (; i < args.length; i++){
+        if (args[i] === "LAST"){
+          if (args[i] === "SLOT_1"){
+            args[i] = this.get_slot(1);
+          }
+          if (args[i] === "SLOT_2"){
+            args[i] = this.get_slot(2);
+          }
+          args[i] = this.lastAns;
+        }
+        product = product * args[i];
+      }
+      this.lastAns = product;
+      return product;
+    }
+  
+    last(){
+      return this.lastAns;
+    }
+    set_slot(n){
+      this.arr.push(this.lastAns);
+      return this.arr[n - 1];
+    }
+  
+    get_slot(n){
+      return this.arr[n - 1];
+    }
+  
+  }
 
-// console.warn(add(1,3));
+  let calculator_instance = new Calculator();
